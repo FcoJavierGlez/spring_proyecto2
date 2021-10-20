378d787f7b14bde24ffb7fe70621e875872e4b5c
FcoJavierGlez <FJGS_86@hotmail.com>
2021-10-20 23:06:54 +0200

    Actualizado

--- src/main/java/Controller/IndexController.java
+++ src/main/java/Controller/IndexController.java
@@ -1,27 +1,23 @@
 package Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 import com.nttdata.bootcamp.proyecto2.config.SaludoProperties;
+import com.nttdata.bootcamp.saludoidioma.classes.Saludo;
 
-//@Controller
 @RestController
-@RequestMapping("/api/")
 public class IndexController {
 
-	@Autowired SaludoProperties s;
+	@Autowired SaludoProperties saludoProperties;
+	@Autowired Saludo saludo;
 	
-	@GetMapping("/saludos")
+	@GetMapping("/saludo")
 	@ResponseBody
-	public String getSaludos() {
-		return "ok";
+	public String getSaludo() {
+		System.out.println( saludoProperties.getIdioma() );
+		return saludo.mensajeSaludo();
 	}
 }
